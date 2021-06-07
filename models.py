36fae3806f29c67f1eec28633c533116a75a78e3
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-07 19:29:16 +0300

    creating a class for articles

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from os import name
+from re import I
 
 
 class Sources:
@@ -14,3 +15,16 @@ class Sources:
         self.category = category
         self.country = country
         self.language = language
+
+class Articles:
+    '''
+    articles class to define articles objects
+    '''
+    def __init__(self, id, author, title, description, url, image, date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
