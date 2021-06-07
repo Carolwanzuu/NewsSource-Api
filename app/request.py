56006df228c0dc16dd649d4ef69c5aef54259cc4
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-07 19:53:06 +0300

    creating api_key, base_url and articles_url and set them to none

--- app/request.py
+++ app/request.py
@@ -1,2 +1,17 @@
 import urllib.request, json
 from models import Sources,Articles
+
+# getting api key
+api_key = None
+
+# getting the news_api base url
+base_url = None
+
+# getting the articles url
+articles_url = None
+
+def configure_request(app):
+    global api_key, base_url, articles_url
+    api_key = app.config['News_Api_Key']
+    base_url = app.config['News_Api_Base_Url']
+    articles_url = app.config['Articles_Base_Url']
