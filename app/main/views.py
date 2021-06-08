50907811033f66e129660b7fc875aa9b57a08b72
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-08 06:46:53 +0300

    updating the main route

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from models import Sources
-from flask import render_template,request,redirect,url_for
+from flask import render_template
 from .import main
 from ..request import get_sources, get_articles
 # from ...modelsmodels import Sources
@@ -9,10 +9,17 @@ def index():
     '''
     view root page function
     '''
-    sources = get_sources('business')
+    business_sources = get_sources('business')
 	technology_sources = get_sources('technology')
 	entertainment_sources = get_sources('entertainment')
+    general_sources = get_sources('general')
     sports_sources = get_sources('sports')
     title = "NEWS"
 
-     return render_template('index.html', title = title, business = Sources, technology = technology_sources, entertainment = entertainment_sources,sports = sports_sources )
+     return render_template('index.html', title = title, business = business_sources, technology = technology_sources, entertainment = entertainment_sources,sports = sports_sources, general = general_sources )
+
+
+@main.route('/news/<id>')
+def newsDetail(id):
+  news = newsdetail(id)
+  return render_template('news.html', news = news)
