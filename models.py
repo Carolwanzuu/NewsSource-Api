e5f2adb1abee9b7bd2b2047840ff476c6a45b959
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-08 08:01:02 +0300

    creating a unittest for articles class

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class Sources:
     '''
     news source class to define sources objects
     '''
-    def __init__(self, id, title, description,url, category,country,language):
+    def __init__(self, id, title, description,url, category,country):
         self.id = id
         self.title = title
         self.description = description
@@ -19,11 +19,10 @@ class Articles:
     '''
     articles class to define articles objects
     '''
-    def __init__(self, id, author, title, description, url, image, date):
-        self.id = id
+    def __init__(self, author, title, description, url, image, date):
+        self.url = url
         self.author = author
         self.title = title
         self.description = description
-        self.url = url
         self.image = image
         self.date = date
