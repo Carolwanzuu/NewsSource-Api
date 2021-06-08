4f2f31b20ca21a240383de807b53bc7d4a49c455
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-08 07:48:03 +0300

    creating a test case for Source class

--- models.py
+++ models.py
@@ -8,13 +8,12 @@ class Sources:
     '''
     def __init__(self, id, title, description,url, category,country,language):
         self.id = id
-        self.name = name
         self.title = title
         self.description = description
         self.url = url
         self.category = category
         self.country = country
-        self.language = language
+    
 
 class Articles:
     '''
